0143b163a84048e85c31f0b1432aa56e2e8d5ee1
nilsding <nilsding@nilsding.org>
2014-06-22 11:15:45 +0200

    the

--- bot.rb
+++ bot.rb
@@ -5,7 +5,7 @@ require "duck_duck_go"
 
 ddg = DuckDuckGo.new
 
-bot = IRC.new("botter", "irc.rrerr.net", "6667", "Botter IRC Bot")
+bot = IRC.new("botter2", "irc.rrerr.net", "6667", "Botter IRC Bot")
 
 IRCEvent.add_callback('endofmotd') do |event|
   bot.add_channel('#lobby')
@@ -16,7 +16,23 @@ IRCEvent.add_callback('privmsg') do |event|
     search_args = event.message.gsub "!search ", ""
     bot.send_message '#lobby', "Searching for: #{search_args}"
   	zci = ddg.zeroclickinfo search_args
-  	bot.send_message  '#lobby', zci.related_topics['_'][0].text
+
+    case zci.type # see https://github.com/andrewrjones/ruby-duck-duck-go/blob/master/lib/duck_duck_go/zero_click_info.rb#L11-L15
+    when 'A'
+      bot.send_message '#lobby', zci.abstract_text
+      bot.send_message '#lobby', "See #{zci.abstract_url.to_s}"
+    when 'D'
+      zci.related_topics['_'].each_with_index do |topic, i|
+        bot.send_message '#lobby', "\x02#{i + 1}.\x02 #{topic.text}" # \x02 - bold text in IRC
+      end
+    when 'C'
+    when 'N'
+    when 'E'
+      bot.send_message '#lobby', zci.answer
+    else
+      bot.send_message '#lobby', "Nothing was found :("
+    end
+
   end
  # bot.send_message '#lobby', event.message
 end
