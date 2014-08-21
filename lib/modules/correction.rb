9d9f5099006e3825d4e1e19c4e4ef59f7854ba7b
nilsding <nilsding@nilsding.org>
2014-08-21 21:04:22 +0200

    corrections can now be stored in the database

--- lib/modules/correction.rb
+++ lib/modules/correction.rb
@@ -15,11 +15,17 @@ class BotterModule::Correction < BotterModule
       subst = match_data[1]
       with = match_data[2]
       
-      $log_db.execute("SELECT message, msg_from, is_action FROM privmsgs WHERE msg_to = ? AND message LIKE ? AND message NOT LIKE \"s/%\" ORDER BY timestamp DESC LIMIT 1", [bot.to(event), "%#{subst}%"]) do |row|
+      $log_db.execute("SELECT message, msg_from, is_action, id FROM privmsgs WHERE msg_to = ? AND message LIKE ? AND message NOT LIKE \"s/%\" ORDER BY timestamp DESC LIMIT 1", [bot.to(event), "%#{subst}%"]) do |row|
+        modified = row[0].gsub(subst, with)
+        id = row[3]
         if row[2] == 0
-          msg = "<#{row[1]}> #{row[0].gsub(subst, with)}"
+          msg = "<#{row[1]}> #{modified}"
         else
-          msg = "* #{row[1]} #{row[0].gsub(subst, with)}"
+          msg = "* #{row[1]} #{modified}"
+        end
+        if @config["save_correction"]
+          puts "updating privmsg id #{id}" if APP_CONFIG["verbose"]
+          $log_db.execute "UPDATE privmsgs SET message = ? WHERE id = ?", [modified, id]
         end
         bot.send_message bot.to(event), "#{@config["banner"]} #{msg}"
       end
